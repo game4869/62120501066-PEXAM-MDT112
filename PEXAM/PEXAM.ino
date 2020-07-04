6e4736c2f0d3e4a2b8fa5585166a59b97195419a
wanchana <wanchana.game@mail.kmutt.ac.th>
2020-07-04 15:06:18 +0700

    2. Complete

--- PEXAM/PEXAM.ino
+++ PEXAM/PEXAM.ino
@@ -1,9 +1,14 @@
+
 void setup() {
-   pinMode(13,OUTPUT);
-
-
+   //pinMode(13,OUTPUT);
+   
+   Serial.begin(9600); 
+   
 }
 void loop() {
-    tone(13,600,300);
-    delay(1000);
+    //tone(13,600,300);
+    //delay(1000);
+
+    Serial.println("Hello MDT!!!");
+    delay(250);
 }
